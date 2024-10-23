1d48014a960d4f9db0607aeee595024f66cf41af
Ajit33 <justajit33@gmail.com>
2024-10-23 15:42:14 +0530

    signup and sign in ui done

---
+++ src/features/auth/components/auth-screen.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { useState } from "react"
+import { SignInFlow } from "../types"
+import { SignInCard } from "./sign-in-card"
+import { SignUpCard } from "./sign-up-card"
+
+export const AuthScreen=()=>{
+    const [authState,setAuthState]=useState<SignInFlow>("signIn")
+    return(
+        <div className=" h-full flex justify-center items-center bg-[#5C3B58]">
+            <div className="md:h-auto md:w-[420px]">
+            {
+                authState==="signIn"? <SignInCard setAuthState={setAuthState} />: <SignUpCard setAuthState={setAuthState} />
+            } 
+            </div>
+        </div>
+    )
+}
